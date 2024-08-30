184eae7a4502f2be0c4a7c0a2c4d3dc3251e287d
rohanku <rohankumar@berkeley.edu>
2024-08-30 13:45:38 -0700

    add zed configuration

--- pull_files.sh
+++ pull_files.sh
@@ -4,9 +4,11 @@ REPO_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 # nvim config
 rm -rf $REPO_DIR/.config/nvim
 rm -rf $REPO_DIR/.config/alacritty
+rm -rf $REPO_DIR/.config/zed
 mkdir -p $REPO_DIR/.config
 cp -ri ~/.config/nvim $REPO_DIR/.config/nvim
 cp -ri ~/.config/alacritty $REPO_DIR/.config/alacritty
+cp -ri ~/.config/zed $REPO_DIR/.config/zed
 
 # tmux config
 cp ~/.tmux.conf $REPO_DIR/.tmux.conf
--- push_files.sh
+++ push_files.sh
@@ -5,10 +5,12 @@ REPO_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 if test -d ~/.config/nvim; then
     mv ~/.config/nvim ~/.config/nvim-bak
     mv ~/.config/alacritty ~/.config/alacritty-bak
+    mv ~/.config/zed ~/.config/zed-bak
 fi
 mkdir -p ~/.config
 cp -ri $REPO_DIR/.config/nvim ~/.config/nvim
 cp -ri $REPO_DIR/.config/alacritty ~/.config/alacritty
+cp -ri $REPO_DIR/.config/zed ~/.config/zed
 
 # tmux config
 cp $REPO_DIR/.tmux.conf ~/.tmux.conf
