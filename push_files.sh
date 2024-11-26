443eb93ec8ee58aba52c353cf7f7bdc9567ba243
Rahul Kumar <rahulkumar@berkeley.edu>
2024-11-26 10:46:59 -0800

    update push files

--- push_files.sh
+++ push_files.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 REPO_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
+set -eufx -o pipefail
+
 # nvim config
 if test -d ~/.config/nvim; then
     mv ~/.config/nvim ~/.config/nvim-bak
-    rm -r ~/.config/nvim
 fi
 if test -d ~/.config/alacritty; then
     mv ~/.config/alacritty ~/.config/alacritty-bak
-    rm -r ~/.config/alacritty
 fi
 if test -d ~/.config/zed; then
     mv ~/.config/zed ~/.config/zed-bak
-    rm -r ~/.config/zed
 fi
 if test -d ~/.config/kitty; then
     mv ~/.config/kitty ~/.config/kitty-bak
-    rm -r ~/.config/kitty
 fi
 if test -f ~/.tmux.conf; then
     mv ~/.tmux.conf ~/.tmux.conf-bak
-    rm ~/.tmux.conf
 fi
 if test -f ~/.zshrc; then
     mv ~/.zshrc ~/.zshrc-bak
-    rm ~/.zshrc
 fi
 
 mkdir -p ~/.config
